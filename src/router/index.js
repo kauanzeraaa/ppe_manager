064d7a03ef2c95071916ee0c7e37708f6c69145a
NathSanFer <158168917+Nathsanfer@users.noreply.github.com>
2026-03-17 11:47:43 -0300

    feat: add routes for new views

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,27 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
+import Dashboard from '../views/Dashboard.vue'
+import Historic from '../views/Historic.vue'
+import Profile from '../views/Profile.vue'
+import Record from '../views/Record.vue'
+import Request from '../views/Request.vue'
+import Settings from '../views/Settings.vue'
+import Stock from '../views/Stock.vue'
 
 // Define as rotas
 const routes = [
     { path : '/', component: Home },
     { path : '/login', component: Login },
-    { path : '/register', component: Register }
+    { path : '/register', component: Register },
+    { path : '/dashboard', component: Dashboard },
+    { path : '/historic', component: Historic },
+    { path : '/profile', component: Profile },
+    { path : '/record', component: Record },
+    { path : '/request', component: Request },
+    { path : '/settings', component: Settings },
+    { path : '/stock', component: Stock }
+
 ]
 
 const router  = createRouter({

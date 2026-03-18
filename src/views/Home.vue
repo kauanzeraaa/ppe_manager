aa3167c4cc3591614c249a610b2002e7856f7293
kauanzeraaa <piacentekauan@gmail.com>
2026-03-18 15:14:30 -0300

    fix - header too close to the section during navigation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,31 +14,25 @@
     <HeaderTemplate />
 
     <div class="body">
-        <h1>Welcome to the Home screen!</h1>
 
         <!-- Section "Home" - aqui é a parte do Miguel, coloquei essa section para, ao clicar em "Home", a tela scollar pra cima -->
         <section id="home" class="nav_home">
-            <h1>SMALL SPACE</h1>
+            <h1>Welcome to the Home screen!</h1>
         </section>
 
         <!-- Section "Our Services" -->
         <section id="services" class="nav_services">
-            <h1>SMALL SPACE</h1>
+            <h1>Our Services</h1>
         </section>
 
         <!-- Section "Contact" -->
         <section id="contact" class="nav_contact">
-            <h1>SMALL SPACE</h1>
+            <h1>Contact</h1>
         </section>
     </div>
 </template>
 
 <style>
-
-/* faz com que o scroll seja mais fluido */
-html {
-    scroll-behavior: smooth;
-}
 
 /* temporario apenas pra teste visual */
 section {
